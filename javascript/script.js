9db282f8db5bdacd2f8795c6f37767df538b3298
Ayoub El Ouali <6029755@mborijnland.nl>
2024-10-21 10:25:08 +0200

    sa

--- javascript/script.js
+++
@@ -1,8 +0,0 @@
-// Get elements
-const burger = document.getElementById('burger');
-const sidebar = document.getElementById('sidebar');
-
-// Toggle sidebar visibility
-burger.addEventListener('click', () => {
-    sidebar.classList.toggle('collapsed');
-});
